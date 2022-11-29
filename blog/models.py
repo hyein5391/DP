eec162a97885912d15bcb4e10ddaa052868e2b16
Wow <110071838+hyein5391@users.noreply.github.com>
2022-11-29 13:57:48 +0900

    Add files via upload

---
+++ blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Facinfo(models.Model):      #클래스 앞글자는 대문자로 명칭하여야 한다
+    fCode = models.IntegerField()
+    fName = models.CharField(max_length=50)
+    fModel = models.CharField(max_length=50)
+    fDesc = models.CharField(max_length=250)
+
+    
+    def __str__(self):
+        return self.fName  #사이트에 Facinfo 의 표시할 내용
